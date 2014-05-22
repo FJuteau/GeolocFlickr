734c867e301fd8c9c821127ada213dde64d77eb4
orsys <orsys@MacBook-de-orsys-6.local>
2014-05-22 09:59:27 +0200

    Initial Commit

---
+++ GeolocFlickr/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GeolocFlickr
+//
+//  Created by orsys on 22/05/2014.
+//  Copyright (c) 2014 François Juteau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ GeolocFlickrTests/GeolocFlickrTests.m
@@ -0,0 +1,34 @@
+//
+//  GeolocFlickrTests.m
+//  GeolocFlickrTests
+//
+//  Created by orsys on 22/05/2014.
+//  Copyright (c) 2014 François Juteau. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface GeolocFlickrTests : XCTestCase
+
+@end
+
+@implementation GeolocFlickrTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
